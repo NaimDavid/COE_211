e0082db2350fc79eeec9521da2d4ab6b7ff5e9d0
Naim David Dankar <naimdavid.dankar@lau.edu>
2022-02-18 15:54:24 +0200

    Initial commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package coe_211;
 
 /**
  *
  * @author naimd
  */
-public class HelloWorld {
+public class COE_211 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello from GitHub!");
+    }
     
 }
