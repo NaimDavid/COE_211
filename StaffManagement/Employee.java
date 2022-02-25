f061a935de9ae83df8d57c5c2fc31ded78b38e9f
NaimDavid <99973481+NaimDavid@users.noreply.github.com>
2022-02-25 18:14:52 +0200

    Add files via upload

---
+++ StaffManagement/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    
+        String first_name;
+        String last_name;
+        int age;
+        double salary;
+        
+        Employee(String first,String last,int a,double s){
+		first_name = first;
+		last_name= last;
+		age = a;
+		salary = s;
+	}
+}
