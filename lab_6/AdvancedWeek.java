d06912b23742311d38cfc782fde2701a459a3450
NaimDavid <99973481+NaimDavid@users.noreply.github.com>
2022-03-11 18:11:14 +0200

    Add files via upload

---
+++ lab_6/AdvancedWeek.java
@@ -0,0 +1,19 @@
+ public class AdvancedWeek {
+    private String d1 = "Monday";
+    private String d2 = "Tuesday";
+    private String d3 = "Wednesday";
+    private String d4 = "Thursday";
+    private String d5 = "Friday";
+    private String d6 = "Saturday";
+    private String d7 = "Sunday"; 
+    
+    private int i = 1;
+    
+     
+   
+    public void printDays() 
+    {
+        System.out.println("The advanced days of the week are:" + "\n" + i++ + ": " + d1 + "," + "\n" + i++ + ": " + d2 + "," + "\n" + i++ + ": " + d3 + ","
+        + "\n" + i++ + ": " + d4 + "," + "\n" + i++ + ": " + d5 + ","+ "\n" + i++ + ": " + d6 + "," + "\n" + i++ + ": " + d7 + ".");
+    }
+}
